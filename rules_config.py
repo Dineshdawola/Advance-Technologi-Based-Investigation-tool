33560acb0e437b7f6523478f8b343308e69c5a97
Cyber Cops Developer <dinesh.dawola@gmail.com>
2026-01-05 17:29:32 +0530

    Cyber Cops Version Fix

--- rules_config.py
+++ rules_config.py
@@ -1,7 +1,6 @@
-# rules_config.py
 def get_system_rules():
     return {
-        "Version": "1.0.0.1.5",  # Yeh line add karna compulsory hai
+        "Version": "1.0.0.1.5",  # Yeh line add karke SAVE karein
         "Identity": "Cyber Cops",
         "Security": "Maximum-Enterprise",
         "Status": "Locked"
